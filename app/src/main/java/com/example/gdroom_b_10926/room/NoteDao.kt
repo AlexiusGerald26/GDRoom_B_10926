fe789707a3c82f8c51220ee9af1f389db9c17358
Gerald <alexiusgeraldjonathan2602@gmail.com>
2022-09-22 09:18:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gdroom_b_10926/room/NoteDao.kt
@@ -0,0 +1,24 @@
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.gdroom_b_10926.room.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    suspend fun addNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM note")
+    suspend fun getNotes() : List<Note>
+
+    @Query("SELECT * FROM note WHERE id =:note_id")
+    suspend fun getNote(note_id: Int) : List<Note>}
